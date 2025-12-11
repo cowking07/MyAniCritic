d4c4ede0c6ef34b0285ea7627e1638277a901f91
anguyen2526 <anguyen94@unomaha.edu>
2025-12-11 14:29:51 -0600

    more uncompleted updates

--- tests/unitTest_BadWordFilter.py
+++ tests/unitTest_BadWordFilter.py
@@ -5,9 +5,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class AC_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        user = 'testuser'
+        pwd = 'AnimeForever'
+        driver.get("http://127.0.0.1:8000/admin")
+        time.sleep(3)
+        elem = driver.find_element(By.ID, "id_username")
+        elem.send_keys(user)
+        elem = driver.find_element(By.ID, "id_password")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000")
+        time.sleep(3)
+
+    def test_AC(self):
+        driver = self.driver
+        driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 if __name__ == '__main__':
