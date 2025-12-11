f7b42b59ac55c95171f5aad70d664afa8a417427
anguyen2526 <anguyen94@unomaha.edu>
2025-12-11 14:10:07 -0600

    unfinished files for unit tests

---
+++ tests/unitTest_EditRating.py
@@ -0,0 +1,13 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
